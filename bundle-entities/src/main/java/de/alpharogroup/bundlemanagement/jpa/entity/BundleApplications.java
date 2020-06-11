6755b8bb181500e2884278d737b9ebc0973a1e7d
astrapi69 <asterios.raptis@gmx.net>
2020-06-11 13:48:26 +0200

    moved entities to own project

---
+++ bundle-entities/src/main/java/de/alpharogroup/bundlemanagement/jpa/entity/BundleApplications.java
@@ -0,0 +1,164 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2015 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.alpharogroup.bundlemanagement.jpa.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import de.alpharogroup.db.entity.enums.DatabasePrefix;
+import de.alpharogroup.db.entity.name.NameEntity;
+import de.alpharogroup.db.entity.name.versionable.VersionableNameUUIDEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * The entity class {@link BundleApplications} is the root of every bundle application. Every entity
+ * of type {@link BundleNames} has as reference to a {@link BundleApplications}. Every entity class
+ * of {@link BundleApplications} has exactly one default locale that is mandatory. If you see it
+ * from the properties file view it is the default properties file is the properties file without
+ * the locale suffix. The entity class {@link BundleApplications} acts as the manager of a
+ * corresponding real application like a web application or an android-app. If the real application
+ * supports only one locale the default locale is enough but if the real application supports more
+ * than one locale object then the supported locale objects are kept in the list
+ * 'supportedLocales'.<br>
+ * Note: The supported locale objects for a real application are mandatory as the default locale.
+ */
+@Entity
+@NamedQueries({
+		@NamedQuery(name = BundleApplications.NQ_FIND_ALL_BY_LANGUAGE_LOCALE, query = "select ba from BundleApplications ba where :languageLocale member of ba.supportedLocales"),
+		@NamedQuery(name = BundleApplications.NQ_FIND_ALL_BY_LANGUAGE_LOCALE_WITH_INNER_JOIN, query = "select ba from BundleApplications ba inner join ba.supportedLocales sl where sl.id = :languageLocaleId") })
+
+@Table(name = BundleApplications.TABLE_NAME, uniqueConstraints = {
+		@UniqueConstraint(name = DatabasePrefix.UNIQUE_CONSTRAINT_PREFIX
+			+ BundleApplications.TABLE_NAME + DatabasePrefix.UNDERSCORE
+			+ NameEntity.COLUMN_NAME_NAME, columnNames = NameEntity.COLUMN_NAME_NAME) }, indexes = {
+					@Index(name = DatabasePrefix.INDEX_PREFIX + BundleApplications.TABLE_NAME
+						+ DatabasePrefix.UNDERSCORE
+						+ NameEntity.COLUMN_NAME_NAME, columnList = NameEntity.COLUMN_NAME_NAME, unique = true) })
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SuperBuilder
+public class BundleApplications extends VersionableNameUUIDEntity implements Cloneable
+{
+
+	/** The Constant BASE_BUNDLE_APPLICATION is the base name of the initial bundle application. */
+	public static final String BASE_BUNDLE_APPLICATION = "base-bundle-application";
+	public static final String NQ_FIND_ALL_BY_LANGUAGE_LOCALE_WITH_INNER_JOIN = "BundleApplications."
+		+ "findAllByLanguageLocaleWithInnerJoin";
+
+	public static final String NQ_FIND_ALL_BY_LANGUAGE_LOCALE = "BundleApplications."
+		+ "findAllByLanguageLocale";
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+
+	public static final String TABLE_NAME = "bundle_applications";
+
+	/**
+	 * The default locale of this bundle application.
+	 */
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.MERGE, CascadeType.REFRESH })
+	@JoinColumn(name = "default_locale_id", nullable = true, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundle_applications_default_locale_id"))
+	LanguageLocales defaultLocale;
+
+	/**
+	 * The supported locale objects that are mandatory for this bundle application.
+	 */
+	@Builder.Default
+	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinTable(name = "bundle_application_language_locales", joinColumns = {
+			@JoinColumn(name = "application_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundle_application_id")) }, inverseJoinColumns = {
+					@JoinColumn(name = "language_locales_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundle_application_language_locales_id")) })
+	Set<LanguageLocales> supportedLocales = new HashSet<>();
+
+	/**
+	 * Adds the given {@link LanguageLocales} to the supported language locales.
+	 *
+	 * @param supportedLocale
+	 *            the supported locale to add
+	 * @return true, if successful
+	 */
+	public boolean addSupportedLanguageLocale(LanguageLocales supportedLocale)
+	{
+		if (supportedLocales == null)
+		{
+			supportedLocales = new HashSet<>();
+		}
+		return supportedLocales.add(supportedLocale);
+	}
+
+	/**
+	 * Checks if the given {@link LanguageLocales} is supported
+	 *
+	 * @param supportedLocale
+	 *            the supported locale
+	 * @return true, if the given {@link LanguageLocales} is supported otherwise false
+	 */
+	public boolean isSupported(LanguageLocales supportedLocale)
+	{
+		return getSupportedLocales().contains(supportedLocale);
+	}
+
+	/**
+	 * Removes the supported language locale.
+	 *
+	 * @param supportedLocale
+	 *            the supported locale
+	 * @return true, if successful
+	 */
+	public boolean removeSupportedLanguageLocale(LanguageLocales supportedLocale)
+	{
+		if (supportedLocales == null)
+		{
+			supportedLocales = new HashSet<>();
+			return false;
+		}
+		return supportedLocales.remove(supportedLocale);
+	}
+
+}
---
+++ bundle-entities/src/main/java/de/alpharogroup/bundlemanagement/jpa/entity/BundleNames.java
@@ -0,0 +1,114 @@
+/**
+ * The MIT License
+ *
+ * Copyright (C) 2015 Asterios Raptis
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package de.alpharogroup.bundlemanagement.jpa.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import de.alpharogroup.db.entity.enums.DatabasePrefix;
+import de.alpharogroup.db.entity.version.VersionableUUIDEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * The entity class {@link BundleNames} holds the data from the {@link BaseNames} and the
+ * {@link LanguageLocales}. If you see it from the properties file view this represents an
+ * properties file. So you can have the default properties file that is the properties file without
+ * the locale suffix and you have for instance a French properties file with the locale suffix _fr.
+ * This would be two entries from this entity class one for the default and one for the French
+ * locale.
+ */
+@Entity
+@Table(name = BundleNames.TABLE_NAME, indexes = {
+		@Index(name = DatabasePrefix.INDEX_PREFIX + BundleNames.TABLE_NAME
+			+ DatabasePrefix.UNDERSCORE
+			+ BundleNames.COLUMN_NAME_BASE_NAME, columnList = BundleNames.COLUMN_NAME_BASE_NAME),
+		@Index(name = DatabasePrefix.INDEX_PREFIX + BundleNames.TABLE_NAME
+			+ DatabasePrefix.UNDERSCORE
+			+ BundleNames.COLUMN_NAME_FILEPATH, columnList = BundleNames.COLUMN_NAME_FILEPATH),
+		@Index(name = DatabasePrefix.INDEX_PREFIX + BundleNames.TABLE_NAME
+			+ DatabasePrefix.UNDERSCORE
+			+ BundleNames.COLUMN_NAME_LOCALE, columnList = BundleNames.COLUMN_NAME_LOCALE),
+		@Index(name = DatabasePrefix.INDEX_PREFIX + BundleNames.TABLE_NAME
+			+ DatabasePrefix.UNDERSCORE
+			+ BundleNames.COLUMN_NAME_OWNER, columnList = BundleNames.COLUMN_NAME_OWNER) })
+@NamedQueries({
+		@NamedQuery(name = BundleNames.NQ_FIND_BY_OWNER, query = "select bn from BundleNames bn where bn.owner=:owner") })
+@Getter
+@Setter
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@SuperBuilder
+public class BundleNames extends VersionableUUIDEntity implements Cloneable
+{
+
+	public static final String COLUMN_NAME_BASE_NAME = "base_name_id";
+	public static final String COLUMN_NAME_FILEPATH = "filepath";
+	public static final String COLUMN_NAME_LOCALE = "locale_id";
+	public static final String COLUMN_NAME_OWNER = "owner_id";
+	/** The Constant for the named query for find BundleNames by the owner. */
+	public static final String NQ_FIND_BY_OWNER = "BundleNames." + "findByOwner";
+	/** Serial Version UID */
+	private static final long serialVersionUID = 1L;
+
+	public static final String TABLE_NAME = "bundlenames";
+
+	/** The base name of this bundle. */
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "base_name_id", nullable = true, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundlenames_base_name_id"))
+	BaseNames baseName;
+
+	/** The optional filepath from this resource bunlde. */
+	@Column(name = "filepath", length = 4096)
+	String filepath;
+
+	/** The locale of this bundle. */
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "locale_id", nullable = true, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundlenames_locale_id"))
+	LanguageLocales locale;
+
+	/** The {@link BundleApplications} that owns this {@link BundleNames} object. */
+	@ManyToOne(fetch = FetchType.EAGER, cascade = { CascadeType.ALL })
+	@JoinColumn(name = "owner_id", nullable = true, referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_bundlenames_owner_id"))
+	BundleApplications owner;
+
+}
