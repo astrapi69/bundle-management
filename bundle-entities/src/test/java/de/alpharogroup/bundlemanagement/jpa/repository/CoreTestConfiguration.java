fe7855d2b5e321ad1684b87de24d75d67e7b432a
astrapi69 <asterios.raptis@gmx.net>
2020-06-22 17:34:00 +0200

    final state of multi-project version with running unit tests to green

--- bundle-entities/src/test/java/de/alpharogroup/bundlemanagement/jpa/repository/CoreTestConfiguration.java
+++
@@ -1,5 +0,0 @@
-package de.alpharogroup.bundlemanagement.jpa.repository;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class CoreTestConfiguration {}
